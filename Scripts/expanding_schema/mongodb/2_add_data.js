400497da82b29007f75fef327c93553a40c94af6
Fanni.Belics <fanni.belics@gmail.com>
2026-01-30 00:26:41 +0100

    Adding sample data to databases

---
+++ Scripts/expanding_schema/mongodb/2_add_data.js
@@ -0,0 +1,6 @@
+db.expandingSchema.insertMany([
+    {field1: 1, field2: "One", field3: false},
+    {field1: 2, field2: "Two", field3: true},
+    {field1: 3, field2: "Three", field3: false},
+    {field1: 4, field2: "Four", field3: true}
+])
---
+++ Scripts/expanding_schema/oracle/2_add_data.sql
@@ -0,0 +1,13 @@
+insert into BASE_ENTITY(id_field, field_1, field_2, field_3)
+values (1, 1, 'One', 0);
+
+insert into BASE_ENTITY(id_field, field_1, field_2, field_3)
+values (2, 2, 'Two', 1);
+
+insert into BASE_ENTITY(id_field, field_1, field_2, field_3)
+values (3, 3, 'Three', 0);
+
+insert into BASE_ENTITY(id_field, field_1, field_2, field_3)
+values (4, 4, 'Four', 1);
+
+commit;
