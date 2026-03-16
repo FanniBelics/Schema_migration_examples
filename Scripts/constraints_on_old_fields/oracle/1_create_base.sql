4645dde5c52c80ddc23a1e0888a8a01864174ffa
Fanni.Belics <fanni.belics@gmail.com>
2026-03-16 17:09:38 +0100

    Constraint on old fields - create starting point - Oracle

---
+++ Scripts/constraints_on_old_fields/oracle/1_create_base.sql
@@ -0,0 +1,11 @@
+-- First we create the base table
+create table base_table(
+    primary_key number constraint base_pk primary key,
+    unique_constraint number(2,0) constraint base_u unique,
+    check_on_static number(2,0) constraint base_static_ck check ( check_on_static > 0 ),
+    check_on_other_field number(2,0),
+    ceiler_for_pervious number(3,0),
+    default_constraint varchar2(50) default 'Default',
+    not_null_field varchar2(50) constraint base_not_null not null
+);
+
--- Scripts/ora_create_users.sql
+++ Scripts/ora_create_users.sql
@@ -7,3 +7,6 @@ grant connect, resource, dba to merge_and_divide_schema;
 
 create user extract_data_schema identified by admin123;
 grant connect, resource, dba to extract_data_schema;
+
+create user constraint_on_old_schema identified by admin123;
+grant connect, resource, dba to extract_data_schema;
