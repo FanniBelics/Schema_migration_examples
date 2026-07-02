73adc515f93fde1397c5687a21c5954973eba3dc
Fanni.Belics <fanni.belics@gmail.com>
2026-07-02 15:05:37 +0200

    Removing not null constraint

--- oracle_large_scale_migrations/script_manager.py
+++ oracle_large_scale_migrations/script_manager.py
@@ -377,6 +377,135 @@ def add_not_null_constraint_to_customer():
         
     record_step(label, results_after_migration_delete)
 
+def remove_not_null_constraint_from_customer():
+    label = "removing_not_null_constraint_from_customer"
+    
+    # print("Exec: before migration select")
+    # with measure(label) as results_before_migration_select:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_select.sql")
+        
+    #     results_before_migration_select[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_before_migration_select'
+    #     results_before_migration_select[json_tags.phase.value] = states.before_migration.value
+        
+    # record_step(label, results_before_migration_select)
+    
+    # print("Exec: before migration insert")
+    # with measure(label) as results_before_migration_insert:
+    #     execute_sql_file("remove_constraint/remove_constraint_before_migration_insert.sql")
+        
+    #     results_before_migration_insert[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_before_migration_insert'
+    #     results_before_migration_insert[json_tags.phase.value] = states.before_migration.value
+        
+    # record_step(label, results_before_migration_insert)
+    
+    # print("Exec: before migration update")
+    # with measure(label) as results_before_migration_update:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_update.sql")
+        
+    #     results_before_migration_update[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_before_migration_update'
+    #     results_before_migration_update[json_tags.phase.value] = states.before_migration.value
+        
+    # record_step(label, results_before_migration_update)
+    
+    # print("Exec: before migration delete")
+    # with measure(label) as results_before_migration_delete:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_delete.sql")
+        
+    #     results_before_migration_delete[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_before_migration_delete'
+    #     results_before_migration_delete[json_tags.phase.value] = states.before_migration.value
+        
+    # record_step(label, results_before_migration_delete)
+    
+    # print("Exec: migration")
+    # with measure(label) as results_migration:
+    #     execute_sql_file("remove_constraint/remove_constraint_in_migration_migration.sql")
+        
+    #     results_migration[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_migration'
+    #     results_migration[json_tags.phase.value] = states.in_migration_state.value
+        
+    # record_step(label, results_migration) 
+    
+    # print("Exec: in migration select")
+    # with measure(label) as results_in_migration_select:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_select.sql")
+        
+    #     results_in_migration_select[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_in_migration_select'
+    #     results_in_migration_select[json_tags.phase.value] = states.in_migration_state.value
+        
+    # record_step(label, results_in_migration_select)
+    
+    # print("Exec: in migration insert")
+    # with measure(label) as results_in_migration_insert:
+    #     execute_sql_file("remove_constraint/remove_constraint_in_migration_insert.sql")
+        
+    #     results_in_migration_insert[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_in_migration_insert'
+    #     results_in_migration_insert[json_tags.phase.value] = states.in_migration_state.value
+        
+    # record_step(label, results_in_migration_insert)
+    
+    # print("Exec: in migration update")
+    # with measure(label) as results_in_migration_update:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_update.sql")
+        
+    #     results_in_migration_update[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_in_migration_update'
+    #     results_in_migration_update[json_tags.phase.value] = states.in_migration_state.value
+        
+    # record_step(label, results_in_migration_update)
+    
+    # print("Exec: in migration delete")
+    # with measure(label) as results_in_migration_delete:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_delete.sql")
+        
+    #     results_in_migration_delete[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_in_migration_delete'
+    #     results_in_migration_delete[json_tags.phase.value] = states.in_migration_state.value
+        
+    # record_step(label, results_in_migration_delete)
+    
+    # print("Exec: closing migration")
+    # with measure(label) as results_after_migration_close_migration:
+    #     execute_sql_file("remove_constraint/remove_constraint_after_migration_close_migration.sql")
+        
+    #     results_after_migration_close_migration[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_after_migration_close_migration'
+    #     results_after_migration_close_migration[json_tags.phase.value] = states.after_migration.value
+
+    # record_step(label, results_after_migration_close_migration)
+    
+    # print("Exec: after migration select")
+    # with measure(label) as results_after_migration_select:
+    #     execute_sql_file("remove_constraint/remove_constraint_migration_select.sql")
+        
+    #     results_after_migration_select[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_after_migration_select'
+    #     results_after_migration_select[json_tags.phase.value] = states.after_migration.value
+        
+    # record_step(label, results_after_migration_select)
+    
+    print("Exec: after migration insert")
+    with measure(label) as results_after_migration_insert:
+        execute_sql_file("remove_constraint/remove_constraint_after_migration_insert.sql")
+        
+        results_after_migration_insert[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_after_migration_insert'
+        results_after_migration_insert[json_tags.phase.value] = states.after_migration.value
+        
+    record_step(label, results_after_migration_insert)
+    
+    print("Exec: after migration update")
+    with measure(label) as results_after_migration_update:
+        execute_sql_file("remove_constraint/remove_constraint_migration_update.sql")
+        
+        results_after_migration_update[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_after_migration_update'
+        results_after_migration_update[json_tags.phase.value] = states.after_migration.value
+
+    record_step(label, results_after_migration_update)
+
+    print("Exec: after migration delete")
+    with measure(label) as results_after_migration_delete:
+        execute_sql_file("remove_constraint/remove_constraint_migration_delete.sql")
+        
+        results_after_migration_delete[json_tags.step_name.value] = 'remove_not_null_constraint_from_customer_after_migration_delete'
+        results_after_migration_delete[json_tags.phase.value] = states.after_migration.value
+        
+    record_step(label, results_after_migration_delete)
+
 if __name__ == "__main__":
     # with measure("test_run") as results:
     #     test_run("test_select.sql")
@@ -390,4 +519,4 @@ if __name__ == "__main__":
     #add_column_to_country()
     #merge_first_and_last_name_in_customer()
     #add_not_null_constraint_to_customer()
-    pass
+    remove_not_null_constraint_from_customer()
